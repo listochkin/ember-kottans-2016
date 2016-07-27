cb4789e4b83ffa37bde2959b8c1ac26ed18eea96
Andrey Listochkin <andrey.listochkin@gmail.com>
2016-07-27 13:50:07 +0300

    initial data to play around

--- app/app.js
+++ app/app.js
@@ -3,6 +3,12 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+window.DATA = [
+  { id: 11, name: 'John', pokemonIds: [1, 2, 3] },
+  { id: 12, name: 'Andrey', pokemonIds: [1, 4, 5] },
+  { id: 13, name: 'Mikhail Bortnyk', pokemonIds: [] }
+];
+
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
